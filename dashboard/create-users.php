3518f0f206eb41465a88eda6c927440928e2dab2
Tajh Seymour <43183381+TajhSeymour@users.noreply.github.com>
2023-10-24 23:45:08 -0400

    test

--- dashboard/create-users.php
+++ dashboard/create-users.php
@@ -92,32 +92,6 @@
                     </div><!--end col-->
                 </div><!-- end row -->
 
-
-
-                <!-- Modal -->
-                <div class="modal fade flip" id="deleteOrder" tabindex="-1" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered">
-                        <div class="modal-content">
-                            <div class="modal-body p-5 text-center">
-                                <lord-icon src="https://cdn.lordicon.com/gsqxdxog.json" trigger="loop" colors="primary:#405189,secondary:#f06548" style="width:90px;height:90px">
-                                </lord-icon>
-                                <div class="mt-4 text-center">
-                                    <h4>You are about to delete a order ?</h4>
-                                    <p class="text-muted fs-14 mb-4">Deleting your order will remove all
-                                        of
-                                        your information from our database.</p>
-                                    <div class="hstack gap-2 justify-content-center remove">
-                                        <button class="btn btn-link link-success fw-medium text-decoration-none" id="deleteRecord-close" data-bs-dismiss="modal"><i class="ri-close-line me-1 align-middle"></i>
-                                            Close</button>
-                                        <button class="btn btn-danger" id="delete-record">Yes, Delete
-                                            It</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!--end modal -->
             </div>
             <!--end card-body-->
         </div>
@@ -185,7 +159,7 @@
                 },
             ],
             "ajax": {
-                "url": "https://oasministersregister.gov.bs/api/dashboard/fetch_data/get_staff_tb_all.php",
+                "url": "api/dashboard/fetch_data/get_staff_tb_all.php",
                 "type": "post"
             },
             "dom": 'lBfrtip',
