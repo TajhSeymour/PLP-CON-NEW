426435ebeba3c2ffd9859bbbe2a4ad8a879f73a4
Tajh Seymour <43183381+TajhSeymour@users.noreply.github.com>
2023-10-26 17:03:54 -0400

    test

--- dashboard/permission/security-check-permission.php
+++ dashboard/permission/security-check-permission.php
@@ -7,7 +7,8 @@ if (
     !(isset($_SESSION['super_admin']) && $_SESSION['super_admin']) &&
     !(isset($_SESSION['security_officer']) && $_SESSION['security_officer'])
     &&
-    !(isset($_SESSION['officer']) && $_SESSION['officer'])
+    !(isset($_SESSION['officer']) && $_SESSION['officer']) &&
+    !(isset($_SESSION['supervisor']) && $_SESSION['supervisor'])
 ) {
     // User is not allowed, redirect to 404.php
     header("Location: 404.php");
