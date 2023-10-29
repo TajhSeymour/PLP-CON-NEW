20aef13da1ee8dff49f194832534b0b9bcfe14c2
Tajh Seymour <43183381+TajhSeymour@users.noreply.github.com>
2023-10-28 21:02:10 -0400

    test

---
+++ api/dashboard/fetch_data/get_registrants_tb_all.php
@@ -0,0 +1,18 @@
+//<?php
+require_once(dirname(__DIR__, 3) . '/database/dbconnection.php');
+
+$query = 'CALL GET_ALL_DELEGATE_REGISTRANTS()';
+$stmt = mysqli_query($connection, $query);
+
+$data = [];
+while ($row = mysqli_fetch_assoc($stmt)) {
+    $data[] = $row;
+}
+
+$results = ["draw" => 1, "recordsTotal" => count($data), "recordsFiltered" => count($data), "data" => $data];
+
+echo json_encode($results);
+
+mysqli_close($connection);
+?>
+
